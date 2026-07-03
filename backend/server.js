a029033befb2fba5497a08213c82c32af19d0876
Ankit soni <asoni.soni12@gmail.com>
2026-07-03 18:26:10 +0530

    add CRUD in backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,59 @@
 import express from "express";
+import { MongoClient } from 'mongodb'
+import bodyParser from "body-parser";
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const app = express()
 const port = 3000;
+app.use(bodyParser.json())
 
-app.get( "/", (req, res) => {
-    res.send("Hello World!");
-})
+// Connection URL
+const url = process.env.MONGO_URI;
+const client = new MongoClient(url);
 
-app.listen(port, () => {
-    console.log("Server is listening on ", port)
-})
+// Database Name
+const dbName = 'passDB';
+
+
+
+async function main() {
+
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+
+    // GET data from MongoDB
+    app.get("/", async (req, res) => {
+        let data = await collection.find({}).toArray()
+        res.send(data)
+    })
+
+    // POST data 
+    app.post( "/", async (req, res) => {
+        const password = req.body;
+        await collection.insertOne(password);
+        res.send(passwords);
+    })
+
+    // DELETE data
+    app.delete( "/", async (req, res) => {
+        const password = req.body;
+        let finalResult = await collection.deleteOne({uid: password.uid})
+        res.send(finalResult);
+    })
+
+    app.listen(port, () => {
+        console.log("Server is listening on ", port)
+    })
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+
